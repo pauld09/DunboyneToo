124842e2b2b266f57f23c821bf32f50519f91d1d
Paul Dunlevy <paul.dunlevy@gm.com>
2022-05-27 17:04:28 +0100

    TestSite

--- public-old/js/firebaseSDK.js
+++ public-old/js/firebaseSDK.js
@@ -7,16 +7,14 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "AIzaSyALYEiyxSG2Lyyvs1P6lrvphGkiOc0mopk",
-authDomain: "dunboyne-too-homepage.firebaseapp.com",
-databaseURL: "https://dunboyne-too-homepage-default-rtdb.europe-west1.firebasedatabase.app",
-projectId: "dunboyne-too-homepage",
-storageBucket: "dunboyne-too-homepage.appspot.com",
-messagingSenderId: "300156802934",
-appId: "1:300156802934:web:948cad4e7a4e929a918c57",
-measurementId: "G-JM7EKWS9BL"
-};
+    apiKey: "AIzaSyD2No7_pj4CuXB-mzJYatdV8kZ4GdjucrA",
+    authDomain: "dunboynetootest.firebaseapp.com",
+    projectId: "dunboynetootest",
+    storageBucket: "dunboynetootest.appspot.com",
+    messagingSenderId: "68502165265",
+    appId: "1:68502165265:web:61d0c5679866d66e11deb0"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
